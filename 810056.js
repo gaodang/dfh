e08ea3b5c31569a99370c8ffaec0ce9c8e073b38
gaodang <40604039+gaodang@users.noreply.github.com>
2020-05-10 00:00:24 +0800

    Create 810056.js

---
+++ 810056.js
@@ -0,0 +1,9 @@
+document.writeln("<iframe src=\'https://down.yaobimei.com:8001/#/?pid=223943\' width=\'100%\' height=\'1200\' frameborder=\'no\' border=\'0\' marginwidth=\'0\' marginheight=\'0\' scrolling=\'no\' allowtransparency=\'yes\'></iframe>");
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ca562f27195099b179f838c70020268d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
